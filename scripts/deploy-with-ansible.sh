3198a31da4b1e60af9def6c599db65365da5563f
devopstrainingschool <107158398+devopstrainingschool@users.noreply.github.com>
2023-04-09 18:52:12 -0500

    Update deploy-with-ansible.sh

--- scripts/deploy-with-ansible.sh
+++ scripts/deploy-with-ansible.sh
@@ -8,11 +8,11 @@ terraform output  -json|jq .mykey.value -r >/tmp/anael_premier.pem && chmod 600 
 
 export ANSIBLE_HOST_KEY_CHECKING=false
 
-ansible-playbook -i /usr/local/bin/terraform.py deploy/install-bastion-hosts.yml && sleep 120 &&  \
-ansible-playbook -i /usr/local/bin/terraform.py deploy/install-postgres-core.yml  && \
-ansible-playbook -i /usr/local/bin/terraform.py deploy/install-postgres-slaves.yml  && \
-ansible-playbook -i /usr/local/bin/terraform.py deploy/install-consul.yml  && \
-ansible-playbook -i /usr/local/bin/terraform.py deploy/install-pgbouncer.yml && \
-ansible-playbook -i /usr/local/bin/terraform.py deploy/install-redis.yml && \
-ansible-playbook -i /usr/local/bin/terraform.py deploy/install-nfs.yml && \
-ansible-playbook -i /usr/local/bin/terraform.py deploy/install-gitlab.yml 
+ansible-playbook -i /usr/local/bin/terraform.py deploy/install-bastion-hosts.yml # && sleep 120 &&  \
+#ansible-playbook -i /usr/local/bin/terraform.py deploy/install-postgres-core.yml  && \
+#ansible-playbook -i /usr/local/bin/terraform.py deploy/install-postgres-slaves.yml  && \
+#ansible-playbook -i /usr/local/bin/terraform.py deploy/install-consul.yml  && \
+#ansible-playbook -i /usr/local/bin/terraform.py deploy/install-pgbouncer.yml && \
+#ansible-playbook -i /usr/local/bin/terraform.py deploy/install-redis.yml && \
+#ansible-playbook -i /usr/local/bin/terraform.py deploy/install-nfs.yml && \
+#ansible-playbook -i /usr/local/bin/terraform.py deploy/install-gitlab.yml 
